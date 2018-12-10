07877ae2ba873e9fd22b99a1f051d5c187187769
unknown <klimovilya@yandex.ru>
2018-12-10 18:39:33 +0400

    trying to add mysql

--- target/generated-sources/annotations/org/ilyaklimov/flightmaster/AppUser_.java
+++ target/generated-sources/annotations/org/ilyaklimov/flightmaster/AppUser_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import org.ilyaklimov.flightmaster.UserRole;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-12-10T15:20:37")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-12-10T15:56:17")
 @StaticMetamodel(AppUser.class)
 public class AppUser_ { 
 
